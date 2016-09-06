717d0d351619f8d5819f3390ea13600a14cf9f15
Erkan Kilmen <ekilmen@gmail.com>
2016-09-06 15:19:37 +0300

    Revert "Revert "Ders2""
    This reverts commit ef7f57a092b5aaa18297bd6eebe2c519c338c12c.

---
+++ Ders2/CodeFirstRelations/CodeFirstRelations/Models/DB/Mdl/UserMsg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstRelations.Models.DB.Mdl
+{
+    public class UserMsg
+    {
+        public int UserMsgId { get; set; }
+        public int UserId { get; set; }
+        public string MessageText { get; set; }
+    }
+}
