682a6165cd614fc6d1086a66ea75c8db4312b99b
Erkan Kilmen <ekilmen@gmail.com>
2016-09-06 15:12:55 +0300

    Ders2
    Mshowto Codefirst Video Eğitim 2. Ders

---
+++ Ders2/CodeFirstRelations/CodeFirstRelations/Models/DB/Mdl/UserAuth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstRelations.Models.DB.Mdl
+{
+    public class UserAuth
+    {
+        public int UserAuthId { get; set; }
+        public string AuthName { get; set; }
+    }
+}
